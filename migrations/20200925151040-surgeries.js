eecb176559f10cc54ba6a281b46c17d403c18427
denis-rossati <denis.rossatiramos@gmail.com>
2021-10-12 20:25:00 -0300

    ajustando nome da coluna 'specalities' na tabela surgeries dentro da migration

--- migrations/20200925151040-surgeries.js
+++ migrations/20200925151040-surgeries.js
@@ -9,7 +9,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      specialty: {
+      speciality: {
         allowNull: false,
         type: Sequelize.STRING,
       },
